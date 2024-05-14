07ae3b3eb516b3f7ea912b74ea1327ffe2c6f618
Tiqur <trevorbrage02@gmail.com>
2024-05-14 14:49:48 -0700

    nixos 60

--- rebuild.sh
+++ rebuild.sh
@@ -9,7 +9,7 @@ sudo nixos-rebuild switch --flake /etc/nixos/#tiqur-nixos
 
 # Commit and push to repo
 git add .
-git commit -m '$(hostname) $(nixos-rebuild list-generations | grep current | cut -d" " -f1)"$(hostname) $(nixos-rebuild list-generations | grep current | cut -d" " -f1)'
+git commit -m "$(hostname) $(nixos-rebuild list-generations | grep current | cut -d" " -f1)"
 git push -u origin main
 
 #cowsay --super "Done!"
