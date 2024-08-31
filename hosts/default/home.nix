a5bda002fe9d9e090a6e34c3896321494ae5ff65
Tiqur <trevorbrage02@gmail.com>
2024-08-31 01:03:27 -0700

    nixos 13

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -15,6 +15,8 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
+  nixpkgs.config.allowUnfree = true;
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
