ad0533265e8ef6e4cb7194718b3e3dddf5c62ded
Tiqur <trevorbrage02@gmail.com>
2024-08-31 00:36:55 -0700

    nixos 11

--- rebuild_and_switch.sh
+++ rebuild_and_switch.sh
@@ -4,4 +4,4 @@ sudo nixos-rebuild switch --flake .#default
 
 git add .
 git commit -m "$(hostname) $(nixos-rebuild list-generations | grep current | cut -d" " -f1)"
-#git push -u origin main
+git push -u origin main
