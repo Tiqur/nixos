796ec29964192398a4d993f0653d4df93d90a9d2
Tiqur <trevorbrage02@gmail.com>
2024-08-31 00:27:04 -0700

    Auto commit on rebuild

--- rebuild_and_switch.sh
+++ rebuild_and_switch.sh
@@ -1,1 +1,7 @@
+nix fmt
+
 sudo nixos-rebuild switch --flake .#default
+
+git add .
+git commit -m "$(hostname) $(nixos-rebuild list-generations | grep current | cut -d" " -f1)"
+#git push -u origin main
