1ccecf3a86eba9bcd58533909106103dcfc98ef2
Tiqur <trevorbrage02@gmail.com>
2024-09-14 13:05:23 -0700

    nixos 29

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -16,6 +16,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  hardware.opengl.enable = true;
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -6,8 +6,6 @@
   home.username = "tiqur";
   home.homeDirectory = "/home/tiqur";
 
-  hardware.opengl.enable = true;
-
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
