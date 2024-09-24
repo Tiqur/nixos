d17b3b1cbe0b5d6151015bf95736c3f2c50aedc5
Tiqur <trevorbrage02@gmail.com>
2024-09-24 16:06:03 -0700

    nixos 31

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -116,9 +116,30 @@
 #    git
   ];
 
-  programs.hyprland = {
+  #programs.hyprland = {
+  #  enable = true;
+  #  xwayland.enable = true;
+  #};
+    services.xserver = {
     enable = true;
-    xwayland.enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+   
+    displayManager = {
+        defaultSession = "none+i3";
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
+     ];
+    };
   };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
