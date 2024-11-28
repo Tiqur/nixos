9526a795cea8985b02f2a6555ec0afc0cdcffa2c
Tiqur <trevorbrage02@gmail.com>
2024-11-27 18:01:03 -0800

    nixos 70

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -44,12 +44,14 @@
     feh
     nixfmt-rfc-style
     xclip
-    jetbrains.rider
-    jetbrains.clion
+    #jetbrains.rider
+    #jetbrains.clion
     osu-lazer-bin
     wlr-randr
     opentabletdriver
     vscode
+    xdg-desktop-portal-wlr
+    fuzzel
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
